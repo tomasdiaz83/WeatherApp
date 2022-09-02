d00171c6f1377e35e6402a3b41fc42fcacfdeb8f
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-09-02 15:45:28 -0500

    Files setup, fetch functioning with API

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var APIkey = "52b2630cb77a5b300aa52ef84d773ad4";
+var city = "houston";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+fetch(queryURL)
+    .then(function (response) {
+        if (!response.ok) {
+            throw console.error();
+        }
+        return response.json();
+    })
+    .then(function(data){
+        
+    })
